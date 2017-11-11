07d67a8b88a3fb5500f4dfcde66db7da8ee9f0d3
Joona Tiinanen <joona_tiinanen@hotmail.com>
2017-11-11 21:23:27 +0200

    Final scaffolding, better "under construction" page

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuetify from 'vuetify'
 
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+// eslint-disable-next-line no-new
 new Vue({
   el: '#app',
   router,
